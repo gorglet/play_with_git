3cee33234bd0413109ba3c5fb44fae945d60de08
Georgie Stuart <g.e.stuart@live.co.uk>
2018-05-08 16:12:19 +0100

    Added text for change number 4

---
+++ git_test.py
@@ -0,0 +1,22 @@
+print "This is just to play with git"
+
+#I can make changes and do version control
+
+#change 1
+print "Ok so this is my first change. Let's commit it..."
+
+#change 2
+def function_one():
+    print "This is a useless function."
+
+function_one()
+
+
+#change 3
+def function_two():
+    print 2 + 1
+    print 3 + 2 + 1
+
+
+#change 4
+"Here is another change!"
